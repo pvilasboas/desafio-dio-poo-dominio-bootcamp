63dc22e15118912bc0b0d837f44b731ec6e655b0
Pedro Roberto <pvilasboas@gmail.com>
2022-08-17 15:26:51 -0300

    Added the classes: Curso, Mentoria and Main

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,25 @@
+import br.com.dio.desafio.dominio.*;
+
+import java.time.LocalDate;
+
+
 public class Main {
 
     public static void main(String[] args) {
+
+        Curso curso1 = new Curso();
+        Mentoria mentoria1 = new Mentoria();
+
+        curso1.setTitulo("curso 1");
+        curso1.setDescricao("descrição do curso 1");
+        curso1.setCargaHoraria(8);
+
+        mentoria1.setTitulo("mentoria 1");
+        mentoria1.setDescricao("descricao metoria 1");
+        mentoria1.setData(LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(mentoria1);
 
     }
 
